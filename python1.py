c06ec97b8ccd279aea9dc21e6b2c0ce08519922a
Shamar <ShamarKnibbs@gmail.com>
2018-04-12 13:48:37 -0400

    python1.py

---
+++ python1.py
@@ -0,0 +1,11 @@
+def make_omelette():
+
+    print("Mixing the ingredients")
+    print("Pouring the mixture")
+    print("Cooking the first side")
+    print("Flipping it!")
+    print("Cooking the other side")
+    omelette="a tasty omelette"
+    return omelette
+
+omelette1= make_omelette()
