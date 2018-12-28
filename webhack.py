9477677a5ba6403a2d00ac5cb4d405f474fd77f4
Shamar Knibbs <shamar.knibbs@stu.bmcc.cuny.edu>
2018-12-28 15:54:34 -0500

    Update webhack.py
    This python script spontaneously opens websites when the user is web browsing

--- webhack.py
+++ webhack.py
@@ -1,9 +1,9 @@
 import webbrowser
 import time
 import random
-while True:
-	sites=random.choice(['google.com','indeed.com','bing.com',])
+while True: 
+	sites=random.choice(['google.com','indeed.com','bing.com',]) // An array of random sites that will open 
 	visit="http://{}".format(sites)
 	webbrowser.open(visit)
-	seconds=random.randrange(3,20)
+	seconds=random.randrange(3,20) // the sites will open for 20 seconds 
 	time.sleep(seconds)
